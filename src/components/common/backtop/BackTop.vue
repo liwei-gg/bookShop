2c5ac4ccf517d765660b11830b5f9c62a266b4db
liwei-gg <237515035@qqcom>
2021-06-02 10:00:47 +0800

    bookShop

---
+++ src/components/common/backtop/BackTop.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="backTop" @click="backtop"></div>
+</template>
+
+<script>
+export default {
+  name: "BackTop",
+  setup(props, {emit}){
+    return{
+      backtop:()=>{
+        emit('bTop');
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+.backTop{
+  width: 40px;
+  height: 40px;
+  position: fixed;
+  bottom: 80px;
+  right: 30px;
+  z-index: 20;
+  box-shadow: 5px 5px 5px #888;
+  border-radius: 50%;
+  background: #f6f6f6 url('~assets/images/tag/Uparrow.png') no-repeat center center;
+}
+</style>
